745ca53b1a6ac48eacb1ccc3b980d38f6f1a09cd
Camille <camille.emma@gmail.com>
2021-12-02 18:42:07 +0100

    day 1

---
+++ day1.py
@@ -0,0 +1,29 @@
+import re
+
+# part 1
+with open('./input_day_1.txt') as file :
+    input = file.read().splitlines()
+    input = [int(ele) for ele in input]
+
+def count_incre(l):
+    count_incr = 0
+    for i in range(1, len(l)):
+        if l[i - 1] - l[i] < 0:
+            count_incr += 1
+    return count_incr
+
+
+print('part 1 : {}'.format(count_incre(input)))
+
+# part 2
+def sliding_window_sum(l, s=3):
+    sliding_list = []
+    for i in range(len(l) - (s-1)):
+        sliding = 0
+        for j in range(s):
+            sliding += l[i+j]
+        sliding_list.append(sliding)
+    return sliding_list
+
+
+print('part 2 : {}'.format(count_incre(sliding_window_sum(input))))
